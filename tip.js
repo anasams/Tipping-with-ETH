e65d569b38720235ce6d2598eb391820391abf56
anasams <122623509+anasams@users.noreply.github.com>
2023-03-14 20:50:23 +0100

    sending an Ethereum transaction
    adding a transaction request to metamask upon submitting of the form

--- tip.js
+++ tip.js
@@ -3,7 +3,17 @@ const form = document.querySelector("form")
 const send = async function(amount) {
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
 
-    alert(accounts)
+    if(accounts.length > 0) {
+        window.ethereum.request({ 
+            method: "eth_sendTransaction",
+            params: [{
+                from: accounts[0],
+                to: "0x84120D763683af356d404DAB50BcBf3F42a2AdE4", 
+                value: "10"
+            }] 
+        })
+    }
+
 }
 
 if(window.ethereum) {
