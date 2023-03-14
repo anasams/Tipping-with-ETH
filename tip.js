1e6eb16cc5ff346a3ed48e8cccefaed7788b939c
anasams <122623509+anasams@users.noreply.github.com>
2023-03-14 21:00:51 +0100

    Ether to Wei with web3.js
    added a conversion: Ether to Wei with web3.js library in order to get the "correct" amount of ether requested from the connected digital wallet

--- tip.js
+++ tip.js
@@ -1,7 +1,11 @@
+const web3 = new Web3(Web3.givenProvider)
+
 const form = document.querySelector("form")
 
 const send = async function(amount) {
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+
+    const wei = web3.utils.toWei(amount, "ether")
 
     if(accounts.length > 0) {
         window.ethereum.request({ 
@@ -9,7 +13,7 @@ const send = async function(amount) {
             params: [{
                 from: accounts[0],
                 to: "0x84120D763683af356d404DAB50BcBf3F42a2AdE4", 
-                value: "10"
+                value: web3.utils.toHex(wei)
             }] 
         })
     }
